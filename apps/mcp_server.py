a098bf4bdde8bc3794ee4bd29a2f28d797508f26
canyon289 <ravinsdrive@gmail.com>
2025-04-05 16:36:53 -0700

    Start MCP Server

--- apps/mcp_server.py
+++ apps/mcp_server.py
@@ -20,23 +20,21 @@ def echo_prompt(message: str) -> str:
     """Create an echo prompt"""
     return f"Please process this message: {message}"
 
-async def run():
-    async with mcp.server.stdio.stdio_server() as (read_stream, write_stream):
-        await server.run(
-            read_stream,
-            write_stream,
-            InitializationOptions(
-                server_name="example",
-                server_version="0.1.0",
-                capabilities=server.get_capabilities(
-                    notification_options=NotificationOptions(),
-                    experimental_capabilities={},
-                ),
-            ),
-        )
+# async def run():
+#     async with mcp.server.stdio.stdio_server() as (read_stream, write_stream):
+#         await server.run(
+#             read_stream,
+#             write_stream,
+#             InitializationOptions(
+#                 server_name="echo",
+#                 server_version="0.1.0",
+#                 capabilities=server.get_capabilities(
+#                     notification_options=NotificationOptions(),
+#                     experimental_capabilities={},
+#                 ),
+#             ),
+#         )
 
 
 if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(run())
+    mcp.run(transport='stdio')
